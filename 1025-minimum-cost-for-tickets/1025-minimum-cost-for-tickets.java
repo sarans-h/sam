b5ba1badb544689032227f19c85f679230f47d47
Saransh Gupta <113707195+sarans-h@users.noreply.github.com>
2024-12-31 18:54:11 +0530

    Time: 1 ms (79.45%), Space: 41 MB (98.32%) - LeetHub

---
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.java
@@ -0,0 +1,23 @@
+class Solution {
+    int dp[];
+    public boolean can(int days[],int run){
+        for(int i:days)if(i==run)return true;
+        return false;
+    }
+    public int help(int run,int days[],int []costs){
+        if(run>days[days.length-1])return 0;
+        if(dp[run]!=-1)return dp[run];
+        if(can(days,run)){
+            int one=costs[0]+help(run+1,days,costs);
+            int seven=costs[1]+help(run+7,days,costs);
+            int trty=costs[2]+help(run+30,days,costs);
+            return dp[run]= Math.min(one,Math.min(seven,trty));
+        }
+        return dp[run]=help(run+1,days,costs);
+    }
+    public int mincostTickets(int[] days, int[] costs) {
+        dp=new int [days[days.length-1]+1];
+        Arrays.fill(dp,-1);
+        return help(1,days,costs);
+    }
+}
