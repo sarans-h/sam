30c055b606a3f27e452cd933c8abd65351c25451
Saransh Gupta <113707195+sarans-h@users.noreply.github.com>
2025-01-10 19:16:26 +0530

    Time: 6 ms (7.09%), Space: 45.5 MB (98.07%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,20 +1,17 @@
 class Solution {
     public int trap(int[] height) {
-        int lmax[]=new int [height.length];
-        int rmax[]=new int [height.length];
-        lmax[0]=height[0];
-        rmax[rmax.length-1]=height[height.length-1];
-        for(int i=1;i<height.length;i++){
-            lmax[i]=Math.max(lmax[i-1],height[i]);
-            rmax[rmax.length-1-i]=Math.max(height[height.length-i-1],rmax[rmax.length-i]);
-        }
-        for(int i=0;i<lmax.length;i++){
-            System.out.println(lmax[i]+" "+rmax[i]);
-        }
         int ans=0;
-        for(int i=0;i<rmax.length;i++){
-            ans+=(Math.min(rmax[i],lmax[i])-height[i]);
-        }
+        Stack<Integer>stack=new Stack<>();
+
+        for(int i=0;i<height.length;i++){
+            while(!stack.isEmpty()&&height[stack.peek()]<height[i]){
+                int h=height[stack.pop()];
+                if(stack.isEmpty())break;
+                int d=i-stack.peek()-1;
+                ans=ans+(Math.min(height[stack.peek()],height[i])-h)*d;
+            }
+            stack.push(i);
+        } 
         return ans;
     }
 }
