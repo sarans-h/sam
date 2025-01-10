e4a6c991df0a068351b6901163da467bf21f2062
Saransh Gupta <113707195+sarans-h@users.noreply.github.com>
2025-01-10 22:12:09 +0530

    Time: 0 ms (100%), Space: 46.4 MB (43.42%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,17 +1,50 @@
+// class Solution {
+//     public int trap(int[] height) {
+//         int lmax[]=new int [height.length];
+//         int rmax[]=new int [height.length];
+//         lmax[0]=height[0];
+//         rmax[rmax.length-1]=height[height.length-1];
+//         for(int i=1;i<height.length;i++){
+//             lmax[i]=Math.max(lmax[i-1],height[i]);
+//             rmax[rmax.length-1-i]=Math.max(height[height.length-i-1],rmax[rmax.length-i]);
+//         }
+//         for(int i=0;i<lmax.length;i++){
+//             System.out.println(lmax[i]+" "+rmax[i]);
+//         }
+//         int ans=0;
+//         for(int i=0;i<rmax.length;i++){
+//             ans+=(Math.min(rmax[i],lmax[i])-height[i]);
+//         }
+//         return ans;
+//     }
+// }
 class Solution {
     public int trap(int[] height) {
+        int l=0;
+        int r=height.length-1;
+        int leftmax=height[0];
+        int rightmax=height[r];
         int ans=0;
-        Stack<Integer>stack=new Stack<>();
-
-        for(int i=0;i<height.length;i++){
-            while(!stack.isEmpty()&&height[stack.peek()]<height[i]){
-                int h=height[stack.pop()];
-                if(stack.isEmpty())break;
-                int d=i-stack.peek()-1;
-                ans=ans+(Math.min(height[stack.peek()],height[i])-h)*d;
+        while(l<=r){
+            if(leftmax<rightmax){
+                if(height[l]>leftmax){
+                    leftmax=height[l];
+                }
+                else{
+                    ans+=(leftmax-height[l]);
+                }
+                l++;
             }
-            stack.push(i);
-        } 
+            else {
+                if(height[r]>rightmax){
+                    rightmax=height[r];
+                }
+                else {
+                    ans+=(rightmax-height[r]);
+                }
+                r--;
+            }
+        }
         return ans;
     }
 }
