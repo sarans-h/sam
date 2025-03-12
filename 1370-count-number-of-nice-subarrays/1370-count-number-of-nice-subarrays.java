c97eb0486b880c80c8d568fc7a1bbdd615da38cc
Saransh Gupta <113707195+sarans-h@users.noreply.github.com>
2025-03-12 13:48:34 +0530

    Time: 10 ms (79.83%), Space: 55.1 MB (67.47%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.java
@@ -1,23 +1,22 @@
- 
-
- class Solution {
-    public static int help(int nums[],int k){
+class Solution {
+    public int help(int []nums,int k){
         int l=0;
         int r=0;
-        int sum=0;
         int ans=0;
-
+        int count=0;
         while(r<nums.length){
-            sum+=nums[r]%2;
-            while(sum>k&&r>=l){
-                sum-=nums[l]%2;
+            count+=(nums[r]%2);
+            while(l<=r&&count>k){
+                count-=(nums[l]%2);
                 l++;
+
             }
-            ans+=(r-l+1);
+            ans+=(r-l+1);//because we are counting <=k so till r to aye hoga and usse km bhi to count krenge tbhi to (k-1) minus kr kr remove kr payennge
             r++;
-        }return ans;
+        }
+        return ans;
     }
-    public int numberOfSubarrays(int[] nums, int goal) {
-        return help(nums,goal)-help(nums,goal-1);
+    public int numberOfSubarrays(int[] nums, int k) {
+        return help(nums,k)-help(nums,k-1);
     }
 }
