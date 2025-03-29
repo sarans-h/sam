a9cdd3c153fc1253698c1949f24448297e518efa
Saransh Gupta <113707195+sarans-h@users.noreply.github.com>
2025-03-29 16:49:23 +0530

    Time: 15 ms (57.24%), Space: 45.4 MB (13.66%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,24 +1,18 @@
 class Solution {
-
-    public int countSubsetWithKsum(int arr[],int ind,int k,int dp[][]){
-        if(ind==0){
-            if(k==0&&arr[ind]==0)return 2;
-            if(k==0||arr[ind]==k)return 1;
-            return 0;
-        }
-        if(dp[ind][k]!=-1)return dp[ind][k];
-        int ntake=countSubsetWithKsum(arr,ind-1,k,dp);
-        int take=0;
-        if(arr[ind]<=k)take=countSubsetWithKsum(arr,ind-1,k-arr[ind],dp);
-        return dp[ind][k]=take+ntake;
-    }
     public int findTargetSumWays(int[] nums, int target) {
-        int total=0;
-        for(int i=0;i<nums.length;i++)total+=nums[i];
-        if(total-target<0)return 0;
-        if((total-target)%2!=0)return 0;
-        int dp[][]=new int [nums.length][(total-target)/2+1];
-for(int i[]:dp)Arrays.fill(i,-1);
-return countSubsetWithKsum(nums,nums.length-1,(total-target)/2,dp);
+        int sum=Arrays.stream(nums).sum();
+        int dp[][]=new int [nums.length][2*sum+1];
+        dp[0][nums[0]+sum]=1;
+        dp[0][-nums[0]+sum]+=1;//because it can be 0 too
+        for(int i=1;i<nums.length;i++){
+            for(int j=-sum;j<sum+1;j++){
+                if(sum+j+nums[i]<=2*sum)
+                dp[i][sum+j]+=dp[i-1][sum+j+nums[i]];
+                if(sum+j-nums[i]>=0){
+                dp[i][sum+j]+=dp[i-1][sum+j-nums[i]];
+                }
+            }
+        }
+        return target+sum>=0&&target+sum<2*sum+1?dp[nums.length-1][target+sum]:0;
     }
 }
