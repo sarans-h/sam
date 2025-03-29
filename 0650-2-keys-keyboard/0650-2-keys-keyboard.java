a5d9c0a21a6b32650fe6f34f1463698037d6855c
Saransh Gupta <113707195+sarans-h@users.noreply.github.com>
2025-03-29 18:41:11 +0530

    Time: 17 ms (39.51%), Space: 40.6 MB (66.84%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.java
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.java
@@ -1,14 +1,13 @@
 class Solution {
-    public int help(int curr ,int prev,int n){
-        if(curr==n)return 0;
-        if(curr>n)return (int)(1e9);
-        int l=2+help(curr*2,curr,n);
-        int r=1+help(curr+prev,prev,n);
-return Math.min(l,r);
-
+    public int help(int current,int previous,int n){
+        if(current == n)return 0;
+        if(current > n)return (int)1e9;
+        int pastingprev=1+help(current+previous,previous,n);
+        int copingAndPastingCurrert=2+help(current*2,current,n);
+        return Math.min(pastingprev,copingAndPastingCurrert);
     }
     public int minSteps(int n) {
-if(n==1)return 0;
-        return 1+ help(1,1,n);
+        if(n==1)return 0;
+        return 1+help(1,1,n);
     }
 }
