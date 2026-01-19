088d3a7ea9aeec525c3666167624ffaea0ca0d58
Saransh Gupta <113707195+sarans-h@users.noreply.github.com>
2026-01-19 22:47:56 +0530

    Time: 3 ms (92.57%), Space: 93.7 MB (13.01%) - LeetHub

--- 2420-find-all-good-indices/2420-find-all-good-indices.cpp
+++ 2420-find-all-good-indices/2420-find-all-good-indices.cpp
@@ -8,19 +8,19 @@ public:
             if(nums[i]<=nums[i-1])before[i]=before[i-1]+1;
             else before[i]=0;
         }
-        for(int i=0;i<nums.size();i++){
-            cout<<before[i]<<" ";
+        // for(int i=0;i<nums.size();i++){
+        //     cout<<before[i]<<" ";
 
-        }
+        // }
         cout<<"\n";
         
         for(int i=nums.size()-1-1;i>=0;i--){
             if(nums[i+1]>=nums[i])after[i]=after[i+1]+1;else after[i]=0;
         }
-        for(int i=0;i<nums.size();i++){
-            cout<<after[i]<<" ";
+        // for(int i=0;i<nums.size();i++){
+        //     cout<<after[i]<<" ";
 
-        }
+        // }
         for(int i=k;i<nums.size()-k;i++){
             if(before[i-1]>=k-1&&after[i+1]>=k-1)ans.push_back(i);
         }return ans;
